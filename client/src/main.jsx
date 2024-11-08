e7eacb8c451081f99dc5a00e310ebac8bc23455f
manish raj <bloodgamer752@gmail.com>
2024-11-09 02:00:24 +0530

    Fix routing issue on Vercel and update API URL

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,39 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.scss";
+// import { AuthContextProvider } from "./context/AuthContext.jsx";
+// import { SocketContextProvider } from "./context/SocketContext.jsx";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <AuthContextProvider>
+//       <SocketContextProvider>
+//         <App />
+//       </SocketContextProvider>
+//     </AuthContextProvider>
+//   </React.StrictMode>
+// );
+
+// client/src/main.jsx
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 import { SocketContextProvider } from "./context/SocketContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <SocketContextProvider>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </SocketContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
+
